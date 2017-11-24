13aa6ae349b2e2988a0f14160d0c7624803b3453
su11003333 <su11003333@gmail.com>
2017-11-24 21:16:02 +0800

    todo-application completed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,58 @@
 var app = new Vue({
   el: '#app',
+
   data: {
-    message: 'Hello Vue!'
-  }
-})
+    todo: null,
+    todoItems: [],
+    editTodo: null,
+  },
+  methods: {
+
+    add(todo) {
+      if(!this.todoIsValid){
+        return;
+      }
+      this.todoItems.push({
+        content: todo,
+        finished: false,
+        isEdited: false,
+      });
+      this.todo = null;
+    },
+    edit(todo){
+      this.editTodo = todo.content;
+           todo.isEdited = true;
+    },
+    remove(todo){
+      this.todoItems = this.todoItems.filter(item =>  item !== todo);
+    },
+    cancelEdit(todo) {
+            this.editTodo = null;
+            todo.isEdited = false;
+        },
+    update(todo) {
+      if (!this.editTodoIsValid) {
+          return;
+      }
+
+            todo.content = this.editTodo;
+            todo.isEdited = false;
+
+            // Reset the edit todo.
+            this.editTodo = null;
+        }
+  },
+  computed: {
+    isBeingEdited() {
+            return this.todoItems
+                    .filter(item => item.isEdited)
+                    .length > 0;
+        },
+    todoIsValid(){
+      return !!this.todo;
+    },
+    editTodoIsValid() {
+            return !!this.editTodo;
+        },
+  },
+});
